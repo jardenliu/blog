9de1ad3e2fcb3d26497aaa512db2817035f4276a
jardenliu <jardenliu1992@gmail.com>
2019-01-02 01:24:25 +0800

    🤞add app default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,72 @@
 <template>
-  <div>
-    <nuxt/>
+  <div class="app">
+    <navbar :config="headerConfig"></navbar>
+    <div class="content">
+      <nuxt />
+      <copyright></copyright>
+    </div>
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Navbar from '~/components/packages/navbar'
+import copyright from '~/components/packages/copyright'
+
+import config from '../config.yml'
+import 'reset-css/reset.css'
+
+import '~/assets/styles/app.scss'
+
+export default {
+  components: {
+    Logo,
+    Navbar,
+    copyright
+  },
+  data() {
+    return {
+      headerConfig: config.header || {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app {
+  display: flex;
+  overflow: hidden;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+.content {
+  width: calc(100vw - 300px);
+  margin-left: 300px;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.page {
+  min-height: calc(100vh - 89px);
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+@media screen and (max-width: 800px) {
+  .app {
+    display: flex;
+    flex-direction: column;
+  }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
+  .page {
+    min-height: calc(100vh - 390px);
+  }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  .article {
+    background: white;
+    margin: 10px 0;
+    padding: 20px;
+  }
+
+  .content {
+    width: calc(100vw);
+    margin: 0;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,11 @@
 <template>
-  <!-- <section class="container"> -->
   <div class="app">
-    <navbar :config="headerConfig"></navbar>
-    <!-- <div class="content">
-      <router-view class="page"></router-view>
-      <copyright></copyright>
-    </div> -->
   </div>
-  <!-- </section> -->
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import Navbar from '~/components/packages/navbar'
-import config from '../config.yml'
-import 'reset-css/reset.css'
-
-import '~/assets/styles/app.scss'
-
-export default {
-  components: {
-    Logo,
-    Navbar
-  },
-  data() {
-    return {
-      headerConfig: config.header || {}
-    }
-  }
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
-.app {
-  display: flex;
-  overflow: hidden;
-}
-
-.content {
-  width: calc(100vw - 300px);
-  margin-left: 300px;
-}
-
-.page {
-  min-height: calc(100vh - 89px);
-}
-
-@media screen and (max-width: 800px) {
-  .app {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .page {
-    min-height: calc(100vh - 390px);
-  }
-
-  .article {
-    background: white;
-    margin: 10px 0;
-    padding: 20px;
-  }
-
-  .content {
-    width: calc(100vw);
-    margin: 0;
-  }
-}
 </style>
